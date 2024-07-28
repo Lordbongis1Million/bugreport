e9bac587850124fdfda599da79432fabf19e96cc
Bogis <dtbowen26@gmail.com>
2024-07-29 11:24:03 +1200

    Changed Message To Subject

--- process_form.php
+++ process_form.php
@@ -12,11 +12,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $first_name = $conn->real_escape_string($_POST['first_name']);
     $last_name = $conn->real_escape_string($_POST['last_name']);
     $email = $conn->real_escape_string($_POST['email']);
-    $message = $conn->real_escape_string($_POST['message']);
+    $subject = $conn->real_escape_string($_POST['subject']);
     
     // SQL query to insert form data into the contacts table
-    $sql = "INSERT INTO contacts (first_name, last_name, email, message, created_at)
-        VALUES ('$first_name', '$last_name', '$email', '$message', NOW())";
+    $sql = "INSERT INTO contacts (first_name, last_name, email, subject, created_at)
+        VALUES ('$first_name', '$last_name', '$email', '$subject', NOW())";
 
     // Execute the query and check if it was successful
     if ($conn->query($sql) === TRUE) {
