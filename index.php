516433665bd79b8ecaa379b6a384bfa46c944b0b
Bogis <dtbowen26@gmail.com>
2024-08-07 10:52:59 +1200

    Id page stuff

--- index.php
+++ index.php
@@ -12,25 +12,31 @@
 // Include the database configuration file
 include 'setup.php';
 
-// Fetch all pages from the database
-$sql = "SELECT * FROM pages where id = 1 ";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  $page = $result->fetch_assoc();
-  
-  // Assign the fields to variables
-  $title1 = $page['title1'];
-  $text1 = $page['text1'];
-  $image1 = $page['image1'];
-  $title2 = $page['title2'];
-  $text2 = $page['text2'];
-  $image2 = $page['image2'];
-  $title3 = $page['title3'];
-  $text3 = $page['text3'];
-  $image3 = $page['image3'];
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  // Fetch the page details from the database
+  $sql = "SELECT * FROM pages WHERE id=$id";
+  $result = $conn->query($sql);
+  // Check if the page exists
+  if ($result->num_rows > 0) {
+    $page = $result->fetch_assoc();
+    
+    // Assign the fields to variables
+    $title1 = $page['title1'];
+    $text1 = $page['text1'];
+    $image1 = $page['image1'];
+    $title2 = $page['title2'];
+    $text2 = $page['text2'];
+    $image2 = $page['image2'];
+    $title3 = $page['title3'];
+    $text3 = $page['text3'];
+    $image3 = $page['image3'];
+  } else {
+    echo "Page not found.";
+    exit;
+  }
 } else {
-  echo "Page not found.";
+  echo "No page ID provided.";
   exit;
 }
 
