56c4abc6d815ddfbf870c225779cfbba48b40d74
Bogis <dtbowen26@gmail.com>
2024-11-29 15:21:39 +1300

    where changed to position

--- send_report.php
+++ send_report.php
@@ -5,14 +5,14 @@ include 'setup.php';
 if ($_SERVER["REQUEST_METHOD"] =="POST") {
 
     // Get form data
-    $where = $_POST['where'];
+    $where = $_POST['position'];
     $rarity = $_POST['rarity'];
     $bug = $_POST['bug'];
 
 
     // Insert report data into the database
-    $sql = "INSERT INTO reports (where, rarity, bug)
-            VALUES ('$where', '$rarity', '$bug')";
+    $sql = "INSERT INTO reports (position, rarity, bug)
+            VALUES ('$position', '$rarity', '$bug')";
 
     if ($conn->query($sql) === TRUE) {
       echo "New report submitted successfully";
